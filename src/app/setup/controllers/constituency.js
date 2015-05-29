b528bfb7b1de7a11a2ca9326d2e72e96be69b88a
Liyosi Collins <collinskivale@gmail.com>
2015-05-29 16:17:47 +0300

    minor changes to county and constituency controllers

--- src/app/setup/controllers/constituency.js
+++ src/app/setup/controllers/constituency.js
@@ -32,49 +32,6 @@
                 }
             ];
         }]
-    )
-    .controller("mfl.setup.controller.constituency.wards", ["$scope","adminApi",
-                "$stateParams",
-        function ($scope, adminApi, $stateParams) {
-            adminApi.constituencies
-                .get($stateParams.const_id)
-                .success(function (data){
-                    $scope.constituency_name = data.name;
-                })
-                .error(function(error){
-                    console.log(error);
-                });
-            $scope.path = [
-                {
-                    name: "Adminstrative area",
-                    route: "admin_units"
-                },
-                {
-                    name: "Constituencies",
-                    route: "admin_units.constituencies"
-                },
-                {
-                    name: "Wards",
-                    route: "admin_units.wards"
-                }
-            ];
-            $scope.title = [
-                {
-                    icon: "fa-map-marker",
-                    name: "{{constituency_name}}'s Wards"
-                }
-            ];
-            $scope.action = [
-                {
-                    func : "onclick=window.history.back()",
-                    class: "action-btn action-btn-primary action-btn-md",
-                    color: "blue",
-                    tipmsg: "Go back",
-                    icon: "fa-arrow-left"
-                }
-            ];
-            $scope.filters = {constituency: $stateParams.const_id};
-        }]
     );
 
 })(angular);
