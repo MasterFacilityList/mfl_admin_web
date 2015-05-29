c137e2e39cbefdd7941abeb6e6eb330752c9c218
JaydroidBot <james@savannahinformatics.com>
2015-05-29 16:39:29 +0300

    Wired in backend to power the charts

--- src/app/dashboard/controllers.js
+++ src/app/dashboard/controllers.js
@@ -25,30 +25,84 @@
     .controller("mfl.dashboard.content", ["$scope", "dashboardApi", 
         function ($scope, dashboardApi) {
             $scope.chart = null;
-            $scope.test = "bnooo";
+            var c3_generate = function (_payload){
+                c3.generate(_payload);
+                $scope.loading = false;
+            };
+            $scope.showGraph = function(_data) {
+                //owner chart
+                var owners_chart = function (_dt) {
+                    var _list = [];
 
+                    angular.forEach(_dt.owner_types, function (item) {
+                        var _item = [item.name, item.count];
+                        _list[_list.length] = _item;
+                    });                    
+
+                    var obj = {
+                        bindto: "#chartowners",
+                        data: {
+                            columns: _list,
+                            type : "pie"
+                        }
+                    };
+                    return obj;
+                };
+
+                var facility_chart = function(_dt) {
+                    var _list = [];
+
+                    angular.forEach(_dt.status_summary, function (item) {
+                        var _item = [item.name, item.count];
+                        _list[_list.length] = _item;
+                    });                    
+
+                    var obj = {
+                        bindto: "#chartfacility",
+                        data: {
+                            columns: _list,
+                            type : "pie"
+                        }
+                    };
+                    return obj;
+                };
+                var top_ten = function (_dt) {
+                    var _list = [];
+
+                    angular.forEach(_dt.county_summary, function (item) {
+                        var _item = [item.name, item.count];
+                        _list[_list.length] = _item;
+                    });                    
+
+                    var obj = {
+                        bindto: "#facilitybar",
+                        data: {
+                            columns: _list,
+                            bar: {
+                                width: 10
+                            },
+                            type : "bar"
+                        }
+                    };
+                    return obj;
+                };
+
+                c3_generate(owners_chart(_data));
+                c3_generate(facility_chart(_data));
+                c3_generate(top_ten(_data));
+            };
+
+            //dashboardApi
             dashboardApi.api.list()
                 .success(function (data) {
-                    console.log(data);
+                    $scope.summary = data;
+                    $scope.loading = true;
+                    $scope.showGraph(data);
                 })
-                .error(function () {
-                    console.log("cool");
+                .error(function (err) {
+                    $scope.loading = false;
+                    $scope.chart_err = err.error;
                 });
-
-            $scope.showGraph = function() {
-                $scope.chart = c3.generate({
-                    bindto: "#chart",
-                    data: {
-                        columns: [
-                            ["data1", 30],
-                            ["data2", 120]
-                        ],
-                        type : "pie"
-                    }
-                });     
-            };
-            $scope.showGraph();
-
         }
     ]);
 
