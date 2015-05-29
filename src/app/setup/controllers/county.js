c300c82e220ed1272212f9bbf3a84eb29876f173
Liyosi Collins <collinskivale@gmail.com>
2015-05-29 12:04:38 +0300

    added setup controllers

---
+++ src/app/setup/controllers/county.js
@@ -0,0 +1,116 @@
+"use strict";
+(function(angular){
+    angular.module("mfl.setup.county.controllers",[
+        "mfl.setup.api"
+    ])
+    .controller("mfl.setup.controller.county.list", ["$scope",
+        function ($scope) {
+            $scope.test = "View administrative area";
+            $scope.path = [
+                {
+                    name: "Adminstrative area",
+                    route: "admin_units"
+                },
+                {
+                    name: "Counties",
+                    route: "admin_unit.counties"
+                }
+            ];
+            $scope.title = [
+                {
+                    icon: "fa-map-marker",
+                    name: "View Counties"
+                }
+            ];
+            $scope.action = [
+                {
+                    func : "onclick=window.history.back()",
+                    class: "action-btn action-btn-primary action-btn-md",
+                    color: "blue",
+                    tipmsg: "Go back",
+                    icon: "fa-arrow-left"
+                }
+            ];
+        }]
+    )
+    .controller("mfl.setup.controller.county.edit", ["$scope",
+        function ($scope) {
+            $scope.path = [
+                {
+                    name: "Adminstrative area",
+                    route: "admin_units"
+                },
+                {
+                    name: "Counties",
+                    route: "admin_unit.counties"
+                },
+                {
+                    name: "Edit County",
+                    route: "admin_unit.view_unit"
+                }
+            ];
+            $scope.title = [
+                {
+                    icon: "fa-map-marker",
+                    name: "View Counties"
+                }
+            ];
+            $scope.action = [
+                {
+                    func : "onclick=window.history.back()",
+                    class: "action-btn action-btn-primary action-btn-md",
+                    color: "blue",
+                    tipmsg: "Go back",
+                    icon: "fa-arrow-left"
+                }
+            ];
+        }]
+    )
+    .controller("mfl.setup.controllers.county.constituency", ["$scope","$stateParams",
+        "adminApi",
+        function ($scope, $stateParams,adminApi) {
+            $scope.count_id = $stateParams.count_id;
+            adminApi.counties
+                .get($stateParams.count_id)
+                .success(function (data){
+                    $scope.county_name = data.name;
+                })
+                .error(function(error){
+                    console.log(error);
+                });
+            $scope.path = [
+                {
+                    name: "Adminstrative area",
+                    route: "admin_units"
+                },
+                {
+                    name: "County",
+                    route: "admin_unit.counties"
+                },
+                {
+                    name: "Constituencies",
+                    route: "admin_units.counties.view_consts"
+                }
+            ];
+
+            $scope.title = [
+                {
+                    icon: "fa-map-marker",
+                    name: "{{county_name}}'s Constituencies"
+                }
+            ];
+            $scope.action = [
+                {
+                    func : "onclick=window.history.back()",
+                    class: "action-btn action-btn-primary action-btn-md",
+                    color: "blue",
+                    tipmsg: "Go back",
+                    icon: "fa-arrow-left"
+                }
+            ];
+            $scope.filters = {county: $stateParams.count_id};
+
+        }]
+    );
+
+})(angular);
