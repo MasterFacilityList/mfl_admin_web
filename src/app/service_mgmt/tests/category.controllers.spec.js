42b16c25877b47ca5e64f275c2acd136c0df856e
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-05-29 15:24:54 +0300

    Add category management tests

--- src/app/service_mgmt/tests/category.controllers.spec.js
+++ src/app/service_mgmt/tests/category.controllers.spec.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    describe("Test service controllers", function () {
+    describe("Test category controllers", function () {
         var ctrl, server_url, httpBackend, rootScope, state, log;
 
         beforeEach(function () {
@@ -9,7 +9,7 @@
             module("mflAdminAppConfig");
             module("mfl.common.forms");
             module("mfl.service_mgmt.services");
-            module("mfl.service_mgmt.controllers.services");
+            module("mfl.service_mgmt.controllers.categories");
 
             inject(["$controller", "SERVER_URL", "$httpBackend", "$rootScope", "$state", "$log",
                 function ($controller, SERVER_URL, $httpBackend, $rootScope, $state, $log) {
@@ -25,113 +25,74 @@
             ]);
         });
 
-        describe("Test service edit controller", function () {
-            it("should get one service and all categories", function () {
+        describe("Test category edit controller", function () {
+            it("should get one category", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$stateParams": {
-                        service_id: 1
+                        category_id: 1
                     },
                     "$scope": scope
                 };
-                httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
-                    .respond(200, {});
-                httpBackend
-                    .expectGET(server_url +
-                               "api/facilities/service_categories/?page_size=1000")
-                    .respond(200, {results: []});
 
-                ctrl("service_edit", data);
+                httpBackend
+                    .expectGET(server_url + "api/facilities/service_categories/1/")
+                    .respond(200, {});
+
+                ctrl("category_edit", data);
 
                 httpBackend.flush();
                 httpBackend.verifyNoOutstandingRequest();
                 httpBackend.verifyNoOutstandingExpectation();
 
-                expect(scope.service).toEqual({});
-                expect(scope.categories).toEqual([]);
+                expect(scope.category).toEqual({});
             });
 
-            it("should log errors on get one service failure", function () {
+            it("should log errors on get one category failure", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$stateParams": {
-                        service_id: 1
+                        category_id: 1
                     },
                     "$scope": scope,
                     "$log": log
                 };
                 httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
-                    .respond(500, {"error": "a"});
-                httpBackend
                     .expectGET(server_url +
-                               "api/facilities/service_categories/?page_size=1000")
-                    .respond(200, {results: []});
+                               "api/facilities/service_categories/1/")
+                    .respond(500, {"error": "a"});
 
                 spyOn(log, "warn");
-                ctrl("service_edit", data);
+                ctrl("category_edit", data);
 
                 httpBackend.flush();
                 httpBackend.verifyNoOutstandingRequest();
                 httpBackend.verifyNoOutstandingExpectation();
 
-                expect(scope.service).toBe(undefined);
+                expect(scope.category).toBe(undefined);
                 expect(log.warn).toHaveBeenCalledWith({"error": "a"});
             });
 
-            it("should log errors on get categories failure", function () {
-                var scope = rootScope.$new();
-                var data = {
-                    "$stateParams": {
-                        service_id: 1
-                    },
-                    "$scope": scope,
-                    "$log": log
-                };
-                httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
-                    .respond(200, {});
-                httpBackend
-                    .expectGET(server_url +
-                               "api/facilities/service_categories/?page_size=1000")
-                    .respond(500, {"error": "a"});
-
-                spyOn(log, "warn");
-                ctrl("service_edit", data);
-
-                httpBackend.flush();
-                httpBackend.verifyNoOutstandingRequest();
-                httpBackend.verifyNoOutstandingExpectation();
-
-                expect(scope.service).toEqual({});
-                expect(scope.categories).toBe(undefined);
-                expect(log.warn).toHaveBeenCalledWith({"error": "a"});
-            });
-
-            it("should save updated service", function () {
+            it("should save updated category", function () {
                 inject(["mfl.common.forms.changes",
                     function (formChanges) {
                         var scope = rootScope.$new();
                         var data = {
                             "$stateParams": {
-                                service_id: 1
+                                category_id: 1
                             },
                             "$state": state,
                             "$scope": scope,
                             "mfl.common.forms.changes": formChanges
                         };
                         httpBackend
-                            .expectGET(server_url + "api/facilities/services/1/")
-                            .respond(200, {});
-                        httpBackend
                             .expectGET(server_url +
-                                       "api/facilities/service_categories/?page_size=1000")
-                            .respond(200, {results: []});
+                                       "api/facilities/service_categories/1/")
+                            .respond(200, {});
 
                         spyOn(formChanges, "whatChanged").andReturn({"name": "get"});
                         spyOn(state, "go");
-                        ctrl("service_edit", data);
+                        ctrl("category_edit", data);
 
                         httpBackend.flush();
                         httpBackend.verifyNoOutstandingRequest();
@@ -140,7 +101,9 @@
                         httpBackend.resetExpectations();
 
                         httpBackend
-                            .expectPATCH(server_url + "api/facilities/services/1/", {"name": "get"})
+                            .expectPATCH(
+                                server_url + "api/facilities/service_categories/1/",
+                                {"name": "get"})
                             .respond(200, {});
                         scope.save();
 
@@ -153,29 +116,25 @@
                 ]);
             });
 
-            it("should not save if service is not updated", function () {
+            it("should not save if category is not updated", function () {
                 inject(["mfl.common.forms.changes",
                     function (formChanges) {
                         var scope = rootScope.$new();
                         var data = {
                             "$stateParams": {
-                                service_id: 1
+                                category_id: 1
                             },
                             "$state": state,
                             "$scope": scope,
                             "mfl.common.forms.changes": formChanges
                         };
                         httpBackend
-                            .expectGET(server_url + "api/facilities/services/1/")
+                            .expectGET(server_url + "api/facilities/service_categories/1/")
                             .respond(200, {});
-                        httpBackend
-                            .expectGET(server_url +
-                                       "api/facilities/service_categories/?page_size=1000")
-                            .respond(200, {results: []});
 
                         spyOn(formChanges, "whatChanged").andReturn({});
                         spyOn(state, "go");
-                        ctrl("service_edit", data);
+                        ctrl("category_edit", data);
 
                         httpBackend.flush();
                         httpBackend.verifyNoOutstandingRequest();
@@ -194,67 +153,67 @@
             });
         });
 
-        describe("Test service delete controller", function () {
-            it("should get the service to delete", function () {
+        describe("Test category delete controller", function () {
+            it("should get the category to delete", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$stateParams": {
-                        service_id: 1
+                        category_id: 1
                     },
                     "$scope": scope
                 };
                 httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
+                    .expectGET(server_url + "api/facilities/service_categories/1/")
                     .respond(200, {});
 
-                ctrl("service_delete", data);
+                ctrl("category_delete", data);
 
                 httpBackend.flush();
                 httpBackend.verifyNoOutstandingRequest();
                 httpBackend.verifyNoOutstandingExpectation();
 
-                expect(scope.service).toEqual({});
+                expect(scope.category).toEqual({});
             });
 
-            it("should log errors on get one service failure", function () {
+            it("should log errors on get one category failure", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$stateParams": {
-                        service_id: 1
+                        category_id: 1
                     },
                     "$scope": scope,
                     "$log": log
                 };
                 httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
+                    .expectGET(server_url + "api/facilities/service_categories/1/")
                     .respond(500, {"error": "a"});
 
                 spyOn(log, "warn");
-                ctrl("service_delete", data);
+                ctrl("category_delete", data);
 
                 httpBackend.flush();
                 httpBackend.verifyNoOutstandingRequest();
                 httpBackend.verifyNoOutstandingExpectation();
 
-                expect(scope.service).toBe(undefined);
+                expect(scope.category).toBe(undefined);
                 expect(log.warn).toHaveBeenCalledWith({"error": "a"});
             });
 
-            it("should delete the service", function () {
+            it("should delete the category", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$stateParams": {
-                        service_id: 1
+                        category_id: 1
                     },
                     "$state": state,
                     "$scope": scope
                 };
                 httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
+                    .expectGET(server_url + "api/facilities/service_categories/1/")
                     .respond(200, {});
 
                 spyOn(state, "go");
-                ctrl("service_delete", data);
+                ctrl("category_delete", data);
 
                 httpBackend.flush();
                 httpBackend.verifyNoOutstandingRequest();
@@ -263,7 +222,7 @@
                 httpBackend.resetExpectations();
 
                 httpBackend
-                    .expectDELETE(server_url + "api/facilities/services/1/")
+                    .expectDELETE(server_url + "api/facilities/service_categories/1/")
                     .respond(204, {});
                 scope.save();
 
@@ -274,23 +233,23 @@
                 expect(state.go).toHaveBeenCalled();
             });
 
-            it("should show error on delete the service failure", function () {
+            it("should show error on delete the category failure", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$stateParams": {
-                        service_id: 1
+                        category_id: 1
                     },
                     "$state": state,
                     "$scope": scope,
                     "$log": log
                 };
                 httpBackend
-                    .expectGET(server_url + "api/facilities/services/1/")
+                    .expectGET(server_url + "api/facilities/service_categories/1/")
                     .respond(200, {});
 
                 spyOn(state, "go");
                 spyOn(log, "warn");
-                ctrl("service_delete", data);
+                ctrl("category_delete", data);
 
                 httpBackend.flush();
                 httpBackend.verifyNoOutstandingRequest();
@@ -299,7 +258,7 @@
                 httpBackend.resetExpectations();
 
                 httpBackend
-                    .expectDELETE(server_url + "api/facilities/services/1/")
+                    .expectDELETE(server_url + "api/facilities/service_categories/1/")
                     .respond(404, {});
 
                 scope.save();
@@ -311,80 +270,27 @@
                 expect(state.go).not.toHaveBeenCalled();
                 expect(log.warn).toHaveBeenCalled();
             });
-
         });
 
-        describe("Test service create controller", function () {
+        describe("Test category create controller", function () {
 
-            it("should get all categories", function () {
-                var scope = rootScope.$new();
-                var data = {
-                    "$scope": scope
-                };
-
-                httpBackend
-                    .expectGET(server_url +
-                               "api/facilities/service_categories/?page_size=1000")
-                    .respond(200, {results: []});
-
-                ctrl("service_create", data);
-
-                httpBackend.flush();
-                httpBackend.verifyNoOutstandingRequest();
-                httpBackend.verifyNoOutstandingExpectation();
-
-                expect(scope.categories).toEqual([]);
-            });
-
-            it("should log errors on get categories failure", function () {
-                var scope = rootScope.$new();
-                var data = {
-                    "$scope": scope,
-                    "$log": log
-                };
-
-                httpBackend
-                    .expectGET(server_url +
-                               "api/facilities/service_categories/?page_size=1000")
-                    .respond(500, {"error": "a"});
-
-                spyOn(log, "warn");
-                ctrl("service_create", data);
-
-                httpBackend.flush();
-                httpBackend.verifyNoOutstandingRequest();
-                httpBackend.verifyNoOutstandingExpectation();
-
-                expect(scope.categories).toBe(undefined);
-                expect(log.warn).toHaveBeenCalledWith({"error": "a"});
-            });
-
-            it("should create the service", function () {
+            it("should create the category", function () {
                 var scope = rootScope.$new();
                 var data = {
                     "$state": state,
                     "$scope": scope
                 };
 
-                httpBackend
-                    .expectGET(server_url +
-                               "api/facilities/service_categories/?page_size=1000")
-                    .respond(200, {results: []});
-
                 spyOn(state, "go");
-                ctrl("service_create", data);
+                ctrl("category_create", data);
 
-                httpBackend.flush();
-                httpBackend.verifyNoOutstandingRequest();
-                httpBackend.verifyNoOutstandingExpectation();
-
-                httpBackend.resetExpectations();
-                scope.service = {
+                scope.category = {
                     "name": "get"
                 };
                 httpBackend
-                    .expectPOST(server_url + "api/facilities/services/", {"name": "get"})
+                    .expectPOST(server_url + "api/facilities/service_categories/", {"name": "get"})
                     .respond(200, {});
+
                 scope.save();
 
                 httpBackend.flush();
