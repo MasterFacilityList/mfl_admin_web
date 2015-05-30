ca6a46aaffeea93c8a081797de03516869385388
jason <jason.wanjohi@savannahinformatics.com>
2015-05-30 17:27:54 +0300

    Setup facilities listing

--- src/app/facilities/modules.js
+++ src/app/facilities/modules.js
@@ -2,6 +2,7 @@
         "use strict";
         angular.module("mfl.facilities", [
             "ui.router",
+            "mfl.facilities.services",
             "mfl.facilities.states"
         ]);
     })(angular);
