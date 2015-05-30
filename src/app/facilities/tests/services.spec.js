f40259323fdd9ce1c630ec7edb0b46415878ee51
jason <jason.wanjohi@savannahinformatics.com>
2015-05-30 18:07:10 +0300

    Added fix for facilities listing

--- src/app/facilities/tests/services.spec.js
+++ src/app/facilities/tests/services.spec.js
@@ -19,7 +19,6 @@
             spyOn(api, "setBaseUrl").andReturn({});
             inject(["mfl.facilities.wrappers", function (wrapper) {
                 expect(wrapper.facilities).toEqual({});
-
                 expect(api.setBaseUrl.calls.length).toBe(1);
                 expect(api.setBaseUrl.calls[0].args[0]).toEqual("api/facilities/facilities/");
             }]);
