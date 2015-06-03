3aca9328a3ee410408271333ecb3bfee183c2c54
Liyosi Collins <collinskivale@gmail.com>
2015-06-03 13:22:24 +0300

    added facility listing and creation controller

---
+++ src/app/facilities/controllers/facility_create.js
@@ -0,0 +1,26 @@
+(function(angular){
+    "use strict";
+    angular.module("mfl.facilities.controllers.create", [])
+    .controller("mfl.facilities.controllers.create.basic", ["$scope", function($scope){
+        console.log("at home controller");
+        $scope.tooltip = {
+            "title": "",
+            "checked": false
+        };
+        $scope.title = [
+            {
+                icon: "fa-building",
+                name: "Create Facility"
+            }
+        ];
+        $scope.action = [
+            {
+                func : "onclick=window.history.back()",
+                class: "action-btn action-btn-primary action-btn-md",
+                color: "blue",
+                tipmsg: "Go back",
+                icon: "fa-arrow-left"
+            }
+        ];
+    }]);
+})(angular);
