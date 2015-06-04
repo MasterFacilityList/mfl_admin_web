5f308fd49a069d1ed607bbb603cb77b9891b052c
Liyosi Collins <collinskivale@gmail.com>
2015-06-04 18:55:20 +0300

    added constituency service

--- src/app/facilities/services.js
+++ src/app/facilities/services.js
@@ -7,6 +7,8 @@
 
     .service("mfl.facilities.wrappers", ["api", function (api) {
         this.facilities = api.setBaseUrl("api/facilities/facilities/");
+        this.filterOptions = api.setBaseUrl("api/common/filtering_summaries/");
+        this.constituencies = api.setBaseUrl("api/common/constituencies");
     }]);
 
 })(angular);
