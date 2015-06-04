b975c0e12d4ea44312c4bcb323f85fcb42819c30
Liyosi Collins <collinskivale@gmail.com>
2015-06-04 18:54:35 +0300

    added settings to controller for combo box

--- src/app/facilities/controllers/facility_create.js
+++ src/app/facilities/controllers/facility_create.js
@@ -1,7 +1,11 @@
 (function(angular){
     "use strict";
-    angular.module("mfl.facilities.controllers.create", [])
-    .controller("mfl.facilities.controllers.create.base", ["$scope", function($scope){
+    angular.module("mfl.facilities.controllers.create", [
+        "mfl.facilities.services"
+    ])
+    .controller("mfl.facilities.controllers.create.base", ["$scope",
+        "safeApply",
+        "mfl.facilities.wrappers", function($scope, safeApply,facilityApi){
         $scope.tooltip = {
             "title": "",
             "checked": false
@@ -21,8 +25,47 @@
                 icon: "fa-arrow-left"
             }
         ];
-        $scope.utils = {
-            data:{county: [{name:"Kiambu", id: 1}, {name:"Nairobi", id: 2}]}
+        $scope.filterData = {
+            constituency: []
+        };
+        $scope.getOptionsData = {
+            getData: function(callback, what){
+                facilityApi.filterOptions.filter({fields: what})
+                .success(function(data){
+                    callback(data[what]);
+                }).error(function(error){
+                    callback([]);
+                    $scope.alert = error.error;
+                });
+            },
+            county: function(callback){
+                $scope.getOptionsData.getData(callback, "county");
+            },
+            facilityType: function(callback){
+                $scope.getOptionsData.getData(callback, "facility_type");
+            },
+            operationStatus: function(callback){
+                $scope.getOptionsData.getData(callback, "operation_status");
+            },
+            owner: function(callback){
+                $scope.getOptionsData.getData(callback, "owner");
+            },
+            constituency: function(callback){
+                $scope.getOptionsData.getData(callback, "constituency");
+            }
+        };
+
+        $scope.events = {
+            county: function(value){
+                facilityApi.constituencies .filter({county:value.id})
+                .success(function(data){
+                    $scope.filterData.constituency = data.results;
+                    console.log($scope.dataCallback);
+                    safeApply($scope);
+                }).error(function(error){
+                    $scope.alert = error.err;
+                });
+            }
         };
 
         // $scope.facility = {id: 1};
