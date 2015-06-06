d1fdfb0ed6a07c532229cbf9601b0ad04118875b
Liyosi Collins <collinskivale@gmail.com>
2015-06-06 13:46:43 +0300

    Sil Grid
    - Fixed search
    - Added key events to search

--- libs/sil_grid/sil_grid.js
+++ libs/sil_grid/sil_grid.js
@@ -124,6 +124,7 @@
                         $scope["q_dump"+name] = {page: $scope.filters.page};
                         delete $scope.filters.page;
                     }
+                    console.log(name, value);
                     self.getData();
                 };
                 self.removeFilter = function(name){
@@ -238,6 +239,7 @@
             restrict: "EA",
             require: "^silGrid",
             templateUrl: SEARCH_TPL,
+            controller: function(){},
             link: function(scope, elem, attrs, gridCtrl){
                 scope.silGrid = {searchQuery:""};
                 scope.silGridSearch = function(clear){
@@ -252,6 +254,31 @@
             }
         };
 
+    })
+    .directive("silGridKeyPress", function () {
+        return {
+            restrict: "A",
+            require: "^silGridSearch",
+            link: function(scope, element){
+                element.bind("keydown keypress", function (event) {
+                //enter key press
+                if(event.which === 13) {
+                    scope.$apply(function (){
+                        scope.silGridSearch(false);
+                    });
+                    event.preventDefault();
+                }
+                //esc key press
+                if(event.which === 27) {
+                    scope.$apply(function (){
+                        scope.silGridSearch(true);
+                    });
+                    event.preventDefault();
+                }
+
+            });
+            }
+        };
     })
     .directive("silGridSort",["$rootScope", function($rootScope){
         return {
