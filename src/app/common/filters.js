cd417239582c4cd015cba9c408092e679c5e7281
jason <jason.wanjohi@savannahinformatics.com>
2015-06-06 13:56:10 +0300

    Added titlecase and boolean filter. Bool filter not working

--- src/app/common/filters.js
+++ src/app/common/filters.js
@@ -8,5 +8,26 @@
             var result = _.findWhere(contact_types, {id : cont_typeId});
             return result.name;
         };
-    }]);
+    }])
+    .filter("titlecase", function() {
+        return function(s) {
+            s = ( s === undefined || s === null ) ? "" : s;
+            return s.toString().toLowerCase().replace( /\b([a-z])/g, function(ch) {
+                return ch.toUpperCase();
+            });
+        };
+    })
+    .filter("boolean", function() {
+        return function(s) {
+            if (s === "true") {
+                return "Yes";
+            }
+            if (s === "false") {
+                return "No";
+            }
+            else {
+                return s;
+            }
+        };
+    });
 })(angular, _);
--- src/app/common/tests/filters.spec.js
+++ src/app/common/tests/filters.spec.js
@@ -1,11 +1,13 @@
 (function () {
     "use strict";
     describe("Filters: testing mfl common filters", function () {
-        var contact_type_filter;
+        var contact_type_filter,titlecase_filter,bool_filter;
         beforeEach(function () {
             module("mfl.common.filters");
             inject(["$filter", function ($filter) {
                 contact_type_filter = $filter("contact_type");
+                titlecase_filter = $filter("titlecase");
+                bool_filter = $filter("boolean");
             }]);
         });
         it("Should have contact_type filter defined", function(){
@@ -29,5 +31,46 @@
             expect(contact_type_filter(
                 cont_type_id, cont_types)).toEqual(solution);
         });
+        it( "Should test titlecase filter",function() {
+            var result;
+            result = titlecase_filter( "i am a test" );
+            expect( result ).toEqual( "I Am A Test" );
+
+            result = titlecase_filter( "I AM A TEST" );
+            expect( result ).toEqual( "I Am A Test" );
+
+            result = titlecase_filter( "" );
+            expect( result ).toEqual( "" );
+
+            result = titlecase_filter( );
+            expect( result ).toEqual( "" );
+
+            result = titlecase_filter( 10 );
+            expect( result ).toEqual( "10" );
+
+            result = titlecase_filter( true );
+            expect( result ).toEqual( "True" );
+
+            result = titlecase_filter( false );
+            expect( result ).toEqual( "False" );
+
+            result = titlecase_filter( null );
+            expect( result ).toEqual( "" );
+        });
+        it("should have the boolean filter return 'Yes' given 'true'", inject(function () {
+            var string = "true", result;
+            result = bool_filter(string, "boolean");
+            expect(result).toEqual("Yes");
+        }));
+        it("should have the boolean filter return 'No' given 'false'", inject(function () {
+            var string = "false", result;
+            result = bool_filter(string, "boolean");
+            expect(result).toEqual("No");
+        }));
+        it("should have the boolean filter return undefined given ''", inject(function () {
+            var string = "", result;
+            result = bool_filter(string, "boolean");
+            expect(result).toEqual("");
+        }));
     });
 })();
