7fac160fcc1620ed89032fd35eff06866fc10c05
Liyosi Collins <collinskivale@gmail.com>
2015-06-08 12:04:18 +0300

    facility workflow routes

--- src/app/facilities/routes/facility_create.js
+++ src/app/facilities/routes/facility_create.js
@@ -10,8 +10,7 @@
             url: "/create",
             views: {
                 "main-content@facilities": {
-                    templateUrl: "facilities/tpls/create/base.tpl.html",
-                    controller: "mfl.facilities.controllers.create.base"
+                    templateUrl: "facilities/tpls/create/base.tpl.html"
                 }
             },
             data : { pageTitle: "Facility Management" }
@@ -19,7 +18,7 @@
         .state("facilities.create.basic", {
                 url: "/basicdetails/:facilityId",
                 views: {
-                    "form-view@facilities.create" : {
+                    "main-content@facilities" : {
                         templateUrl : "facilities/tpls/create/basic.tpl.html",
                         controller: "mfl.facilities.controllers.create.base"
                     }
@@ -28,7 +27,7 @@
         .state("facilities.create.address", {
                 url: "/address/:facilityId",
                 views: {
-                    "form-view@facilities.create" : {
+                    "main-content@facilities" : {
                         templateUrl : "facilities/tpls/create/address.tpl.html",
                         controller: "mfl.facilities.controllers.create.address"
                     }
@@ -37,16 +36,17 @@
         .state("facilities.create.contacts", {
                 url: "/contacts/:facilityId",
                 views: {
-                    "form-view@facilities.create" : {
+                    "main-content@facilities" : {
                         templateUrl : "facilities/tpls/create/contacts.tpl.html",
                         controller: "mfl.facilities.controllers.create.contacts"
                     }
+
                 }
             })
         .state("facilities.create.services", {
                 url: "/services/:facilityId",
                 views: {
-                    "form-view@facilities.create" : {
+                    "main-content@facilities" : {
                         templateUrl : "facilities/tpls/create/services.tpl.html",
                         controller: "mfl.facilities.controllers.create.services"
                     }
