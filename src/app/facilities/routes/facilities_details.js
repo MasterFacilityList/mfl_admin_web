cda6b29113f22219271b6f1264a18c56bb69f616
Liyosi Collins <collinskivale@gmail.com>
2015-06-10 17:02:16 +0300

    edited facility details routes to add routes for facility action

--- src/app/facilities/routes/facilities_details.js
+++ src/app/facilities/routes/facilities_details.js
@@ -25,6 +25,45 @@
                 }
             },
             data : { pageTitle: "Facility Details" }
-        });
+        })
+        .state("facilities_view.approve", {
+                url: "/approve/",
+                views: {
+                    "main-content@facilities_view" : {
+                        templateUrl : "facilities/tpls/view/approve.tpl.html",
+                        controller: "mfl.facilities.controllers.view.approve"
+                    }
+                }
+            })
+        .state("facilities_view.add_unit", {
+                url: "/add_unit/",
+                views: {
+                    "main-content@facilities_view" : {
+                        templateUrl : "facilities/tpls/view/add_unit.tpl.html",
+                        controller: "mfl.facilities.controllers.view.add_unit"
+                    }
+                }
+            })
+
+        .state("facilities_view.upgrade", {
+                url: "/upgrade/",
+                views: {
+                    "main-content@facilities_view" : {
+                        templateUrl : "facilities/tpls/view/upgrade.tpl.html",
+                        controller: "mfl.facilities.controllers.view.upgrade"
+                    }
+                }
+            })
+
+        .state("facilities_view.mutate_op_status", {
+                url: "/mutate_op_status/",
+                views: {
+                    "main-content@facilities_view" : {
+                        templateUrl : "facilities/tpls/view/mutate_op_status.tpl.html",
+                        controller: "mfl.facilities.controllers.view.mutate_op_status"
+                    }
+                }
+            })
+        ;
     }]);
 })(angular);
