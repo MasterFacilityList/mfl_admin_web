0fb23acfc1c14f12dad742489d1c1712fbbdaa15
Liyosi Collins <collinskivale@gmail.com>
2015-06-10 12:50:58 +0300

    added side nav on facility details view

--- src/app/facilities/routes/facilities_details.js
+++ src/app/facilities/routes/facilities_details.js
@@ -6,15 +6,25 @@
     ])
     .config(["$stateProvider", function ($stateProvider) {
         $stateProvider
-        .state("facilities.view", {
-            url: "/view/:facilityId",
+        .state("facilities_view", {
+            url: "/facility/:facilityId",
             views: {
-                "main-content@facilities": {
+                "main": {
+                    templateUrl: "facilities/tpls/view/base.tpl.html"
+                },
+                "header@facilities_view": {
+                    controller: "mfl.common.controllers.header",
+                    templateUrl: "common/tpls/header.tpl.html"
+                },
+                "main-content@facilities_view": {
                     templateUrl: "facilities/tpls/view/view.tpl.html",
                     controller: "mfl.facilities.controllers.view.base"
+                },
+                "sidebar@facilities_view": {
+                    templateUrl: "facilities/tpls/view/sidebar.tpl.html"
                 }
             },
-            data : { pageTitle: "Facility Management" }
+            data : { pageTitle: "Facility Details" }
         });
     }]);
 })(angular);
