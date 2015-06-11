46f46c3f0954dcc8861c195901502b46a9c1959a
Liyosi Collins <collinskivale@gmail.com>
2015-06-11 19:28:43 +0300

    added controller for facility status and type

--- src/app/facilities/controllers/facility_home.js
+++ src/app/facilities/controllers/facility_home.js
@@ -24,5 +24,51 @@
                 icon: "fa-user-plus"
             }
         ];
+    }])
+
+    .controller("mfl.facilities.controllers.home.facility_type", ["$scope", function($scope){
+        $scope.tooltip = {
+            "title": "",
+            "checked": false
+        };
+        $scope.title = [
+            {
+                icon: "fa-building",
+                name: "Manage Facility Type"
+            }
+        ];
+        $scope.action = [
+            {
+                func : "ui-sref='facilities.create.basic' " +
+                        "has-permission='users.add_mfluser' ",
+                class: "action-btn action-btn-info action-btn-md",
+                color: "blue",
+                tipmsg: "New Facility",
+                icon: "fa-user-plus"
+            }
+        ];
+    }])
+
+    .controller("mfl.facilities.controllers.home.facility_status", ["$scope", function($scope){
+        $scope.tooltip = {
+            "title": "",
+            "checked": false
+        };
+        $scope.title = [
+            {
+                icon: "fa-building",
+                name: "Manage Facility Status"
+            }
+        ];
+        $scope.action = [
+            {
+                func : "ui-sref='facilities.create.basic' " +
+                        "has-permission='users.add_mfluser' ",
+                class: "action-btn action-btn-info action-btn-md",
+                color: "blue",
+                tipmsg: "New Facility",
+                icon: "fa-user-plus"
+            }
+        ];
     }]);
 })(angular);
