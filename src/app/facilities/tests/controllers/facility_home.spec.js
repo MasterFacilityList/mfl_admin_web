3bf4a446f77f0945de10356cd948a0d4a9be27c6
Liyosi Collins <collinskivale@gmail.com>
2015-06-12 17:37:59 +0300

    added tests for facility service and facility type view controller

--- src/app/facilities/tests/controllers/facility_home.spec.js
+++ src/app/facilities/tests/controllers/facility_home.spec.js
@@ -2,16 +2,17 @@
     "use strict";
     describe("Facility home controllers test suite", function(){
         var createController, $scope, $stateParams, $httpBackend, $state, facilitiesApi;
-        var SERVER_URL;
+        var SERVER_URL, formService;
 
         beforeEach(function(){
             module("mflAdminApp");
             inject(["$rootScope", "$controller", "$stateParams", "$httpBackend",
-                   "$state", "mfl.facilities.wrappers","SERVER_URL",
+                   "$state", "mfl.facilities.wrappers","SERVER_URL","mfl.common.forms.changes",
                    function($rootScope, $controller,_$stateParams,
-                    _$httpBackend, _$state, _facilityApi, frm, _SERVER_URL){
+                    _$httpBackend, _$state, _facilityApi, frm, _SERVER_URL, frmService){
                 $scope = $rootScope.$new();
                 SERVER_URL = _SERVER_URL;
+                formService = frmService;
                 $httpBackend = _$httpBackend;
                 $stateParams = _$stateParams;
                 $state = _$state;
@@ -53,6 +54,39 @@
            function(){
                 var ctrl = createController("mfl.facilities.controllers.home.facility_type", {});
                 expect(ctrl).toBeDefined();
+            });
+
+        it("should have `mfl.facilities.controllers.home.facility_type.create` defined",
+           function(){
+                var ctrl = createController(
+                    "mfl.facilities.controllers.home.facility_type.create", {});
+                expect(ctrl).toBeDefined();
+            });
+        it("should update facility Type: fail",function(){
+                var dt = {
+                    $stateParams: {id: 1}
+                };
+                var res = {msg: "Ok"};
+                var form = {name : "Antony"};
+                spyOn($state, "go");
+                $httpBackend.expectGET(SERVER_URL+"api/facilities/facility_types/1/").respond(
+                200, res);
+                $httpBackend.expectPATCH(SERVER_URL+"api/facilities/facility_types/1/").respond(
+                200, res);
+                createController("mfl.facilities.controllers.home.facility_type.create", dt);
+                $scope.updateFacilityType(1, form);
+                expect($state.go).not.toHaveBeenCalledWith("facilities.facility_type");
+            });
+
+        it("should create facility Type: sucess",function(){
+                var res = {msg: "Ok"};
+                var form = {name : "Antony"};
+                spyOn($state, "go");
+                $httpBackend.expectPOST(SERVER_URL+"api/facilities/facility_types/1/").respond(
+                200, res);
+                createController("mfl.facilities.controllers.home.facility_type.create");
+                $scope.createFacilityType(form);
+                expect($state.go).not.toHaveBeenCalledWith("facilities.facility_type");
             });
     });
 })(describe);
