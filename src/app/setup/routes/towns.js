cf09d17e7e6c9e3b9c35a50bd2631645f704049c
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-06-12 15:07:44 +0300

    Adding crud for towns

--- src/app/setup/routes/towns.js
+++ src/app/setup/routes/towns.js
@@ -4,7 +4,7 @@
 
     .config(["$stateProvider", function ($stateProvider) {
         $stateProvider
-        .state("setup.towns", {
+            .state("setup.towns", {
                 url: "/towns",
                 views: {
                     "body@setup" : {
@@ -15,7 +15,44 @@
                         templateUrl: "setup/tpls/towns/towns.tpl.html"
                     }
                 }
-            });
+            })
+            .state("setup.town_edit", {
+                url: "/towns/edit/:town_id",
+                views: {
+                    "body@setup" : {
+                        templateUrl: "setup/tpls/dashboard/body.tpl.html"
+                    },
+                    "main-content@setup.town_edit": {
+                        controller: "mfl.setup.controller.town.edit",
+                        templateUrl: "setup/tpls/towns/towns.edit.tpl.html"
+                    }
+                }
+            })
+            .state("setup.town_create", {
+                url: "/towns/create",
+                views: {
+                    "body@setup" : {
+                        templateUrl: "setup/tpls/dashboard/body.tpl.html"
+                    },
+                    "main-content@setup.town_create": {
+                        controller: "mfl.setup.controller.town.create",
+                        templateUrl: "setup/tpls/towns/towns.edit.tpl.html"
+                    }
+                }
+            })
+            .state("setup.town_delete", {
+                url: "/towns/delete/:town_id",
+                views: {
+                    "body@setup" : {
+                        templateUrl: "setup/tpls/dashboard/body.tpl.html"
+                    },
+                    "main-content@setup.town_delete": {
+                        controller: "mfl.setup.controller.town.delete",
+                        templateUrl: "setup/tpls/towns/towns.delete.tpl.html"
+                    }
+                }
+            })
+            ;
     }]);
 })(angular);
 
