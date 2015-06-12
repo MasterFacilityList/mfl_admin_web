e577909b9d9ad14fcd6cf934933153df6fb261c8
Liyosi Collins <collinskivale@gmail.com>
2015-06-12 18:03:40 +0300

    added facility status create/view controller

--- src/app/facilities/controllers/facility_home.js
+++ src/app/facilities/controllers/facility_home.js
@@ -128,13 +128,75 @@
         ];
         $scope.action = [
             {
-                func : "ui-sref='facilities.create.basic' " +
+                func : "ui-sref='facilities.facility_status_create' " +
                         "has-permission='users.add_mfluser' ",
                 class: "action-btn action-btn-info action-btn-md",
                 color: "blue",
-                tipmsg: "New Facility",
-                icon: "fa-user-plus"
+                tipmsg: "New Facility Status",
+                icon: "fa-plus"
             }
         ];
-    }]);
+    }])
+
+    .controller("mfl.facilities.controllers.home.facility_status.create", ["$scope","$state",
+     "$stateParams","mfl.facilities.wrappers","mfl.common.forms.changes",
+        function($scope, $state, $stateParams, facilityApi, formChanges){
+            var titles = {
+                edit: [
+                    {
+                        icon: "fa-edit",
+                        name: "Edit Facility Status"
+                    }
+                ],
+                create:  [
+                    {
+                        icon: "fa-plus-circle",
+                        name: "New Facility Status"
+                    }
+                ]
+            };
+            var actions = {
+                defaults: [{
+                    func : "onclick='window.history.back()'",
+                    class: "action-btn action-btn-primary action-btn-md",
+                    color: "blue",
+                    tipmsg: "Go Back",
+                    icon: "fa-arrow-left"
+                }],
+                create: [{
+
+                }],
+                edit:[
+                    {
+                        func : ""+
+                        " mfl-delete api='mfl.facilities.wrappers' api-key='facility_status'"+
+                        " resource-id='"+$stateParams.id+"' "+
+                        "resource-name='Facility Status'"+
+                        " on-success-url='facilities.facility_status'",
+                        class: "action-btn action-btn-danger action-btn-md",
+                        color: "blue",
+                        tipmsg: "Delete Facility Status",
+                        icon: "fa-trash"
+                    }
+                ]
+            };
+            facilityApi.utils.setActions(
+                $scope, $stateParams,facilityApi.facility_status, titles,actions);
+            $scope.updateFacilityStatus = function(id, frm){
+                facilityApi.utils.update(
+                    id, frm,
+                    facilityApi.facility_status,
+                    $scope, $state, "facilities.facility_status",
+                    formChanges
+                );
+            };
+            $scope.createFacilityStatus = function(facilityStatus){
+                facilityApi.utils.create(
+                    facilityStatus,
+                    facilityApi.facility_status,
+                    $scope, $state, "facilities.facility_status"
+                );
+            };
+        }]
+    );
 })(angular);
