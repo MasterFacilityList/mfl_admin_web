abed4c33fe98cf11dd5022ce11a0ef09fd26de3b
bmarika <marika@savannahinformatics.com>
2015-06-23 11:25:01 +0300

    Add facility listing services and tests

---
+++ src/app/facility_mgmt/tests/services.spec.js
@@ -0,0 +1,24 @@
+(function () {
+    "use strict";
+
+    describe("Test facility_mgmt module services", function () {
+        beforeEach(function(){
+            module("mflAdminAppConfig");
+            module("api.wrapper");
+            module("mfl.facility_mgmt.services");
+        });
+
+        describe("Test api wrappers", function() {
+
+            it("should have wrappers defined", function() {
+                inject(["mfl.facility_mgmt.services.wrappers",function(wrappers){
+                    expect(wrappers.facilities.apiBaseUrl).toEqual(
+                        "api/facilities/facilities_list/");
+                  
+                }]);
+
+            });
+        });
+    });
+
+})();
