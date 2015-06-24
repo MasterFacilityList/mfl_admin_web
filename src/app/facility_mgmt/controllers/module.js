9550a605b55d88216909034910f1d38a55b9ead4
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-06-24 15:11:12 +0300

    Add upgrade & downgrade context actions in facility detail view

--- src/app/facility_mgmt/controllers/module.js
+++ src/app/facility_mgmt/controllers/module.js
@@ -35,7 +35,7 @@
                     func: "require-permission='facilities.add_facilityapproval' " +
                           "ng-if='!facility.is_approved || facility.has_edits' " +
                           "ui-sref='facilities.facility_edit({facility_id: facility.id})'",
-                    class: "action-btn action-btn-md action-btn-info",
+                    class: "action-btn action-btn-md action-btn-primary",
                     icon: "fa-check",
                     tipmsg: "Approve"
                 },
@@ -43,7 +43,7 @@
                     func: "require-permission='facilities.change_facility' " +
                           "ng-if='login_user.is_national' " +
                           "ui-sref='facilities.facility_edit({facility_id: facility.id})'",
-                    class: "action-btn action-btn-md action-btn-info",
+                    class: "action-btn action-btn-md action-btn-primary",
                     icon: "fa-upload",
                     tipmsg: "Publish"
                 },
@@ -51,9 +51,21 @@
                     func: "require-permission='facilities.add_facilityregulationstatus' " +
                           "ng-if='login_user.regulator' " +
                           "ui-sref='facilities.facility_edit({facility_id: facility.id})'",
-                    class: "action-btn action-btn-md action-btn-info",
+                    class: "action-btn action-btn-md action-btn-primary",
                     icon: "fa-legal",
                     tipmsg: "Regulate"
+                },
+                {
+                    func: "ui-sref='facilities.facility_edit({facility_id: facility.id})'",
+                    class: "action-btn action-btn-md action-btn-primary",
+                    icon: "fa-level-up",
+                    tipmsg: "Upgrade"
+                },
+                {
+                    func: "ui-sref='facilities.facility_edit({facility_id: facility.id})'",
+                    class: "action-btn action-btn-md action-btn-primary",
+                    icon: "fa-level-down",
+                    tipmsg: "Downgrade"
                 }
             ];
         }]
