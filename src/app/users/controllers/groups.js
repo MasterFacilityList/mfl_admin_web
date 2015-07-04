b0e505a6c06ac5a536c1b7b97bac0ecb145ecbf5
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-07-04 20:19:39 +0300

    Adding rbac to users

--- src/app/users/controllers/groups.js
+++ src/app/users/controllers/groups.js
@@ -15,7 +15,8 @@
             $scope.action = [
                 {
                     func : "ui-sref='groups.group_create' " +
-                            "requires-permission='auth.add_group' ",
+                            "requires-permission='auth.add_group' " +
+                            "requires-user-feature='is_staff'",
                     class: "action-btn action-btn-primary action-btn-md",
 
                     tipmsg: "New Group",
@@ -81,7 +82,8 @@
             $scope.action = [
                 {
                     func : "ui-sref='groups.group_edit.delete' " +
-                           "requires-permission='auth.delete_group'" ,
+                           "requires-permission='auth.delete_group'"  +
+                           "requires-user-feature='is_staff'",
                     class: "action-btn action-btn-danger action-btn-md",
 
                     tipmsg: "Delete Group",
