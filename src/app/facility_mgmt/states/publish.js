6077bc90cc10dfbd252652ce45d66f43938fc21e
jason <jason.wanjohi@savannahinformatics.com>
2015-07-04 19:13:34 +0300

    Restyling of publish

--- src/app/facility_mgmt/states/publish.js
+++ src/app/facility_mgmt/states/publish.js
@@ -9,6 +9,9 @@
         $stateProvider.state("facilities.facility_edit.publish", {
             url: "publish/",
             views: {
+                "tab-header@facilities.facility_edit": {
+                    templateUrl: "facility_mgmt/tpls/publish.tabheaders.tpl.html"
+                },
                 "publish@facilities.facility_edit": {
                     templateUrl: "facility_mgmt/tpls/facility_publish.tpl.html",
                     controller: "mfl.facility_mgmt.controllers.facility_publish"
