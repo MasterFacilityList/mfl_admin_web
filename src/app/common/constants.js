3bedf78b47b135e0442c9587210d7306c357957d
ANTONY <antony.owaga@savannahinformatics.com>
2015-07-15 10:48:57 +0300

    Adding facility notifications checkpoint

--- src/app/common/constants.js
+++ src/app/common/constants.js
@@ -11,7 +11,13 @@
         fetch_contacts : "failed to fetch contacts",
         fetching_services : "failed to fetch services",
         services : "failed to update services",
-        delete_services : "failed to delete service"
-
+        delete_services : "failed to delete service",
+        fetch_units : "failed to fetch facility units",
+        units : "failed to update units",
+        delete_units : "failed to delete units",
+        location : "failed to update facility location details",
+        geolocation : "failed to update facility geolocation details",
+        geocodes : "failed to fetch facility geocoordinates",
+        ward : "failed to fetch facility's ward geocoordinates"
     });
 })(angular);
