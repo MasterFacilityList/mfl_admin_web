2b4ce30e2f4a4d7bea14773c2e9b09518a914dd6
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-07-20 17:47:46 +0300

    adding facility cover report printing

--- src/app/facility_mgmt/services.js
+++ src/app/facility_mgmt/services.js
@@ -1,7 +1,10 @@
 (function (angular) {
     "use strict";
 
-    angular.module("mfl.facility_mgmt.services", ["api.wrapper"])
+    angular.module("mfl.facility_mgmt.services", [
+        "api.wrapper",
+        "mfl.auth.oauth2"
+    ])
 
     .service("mfl.facility.multistep.service", [function () {
         this.facilityObject = function () {
@@ -49,37 +52,51 @@
         };
     }])
 
-    .service("mfl.facility_mgmt.services.wrappers", ["api", function (api) {
-        this.facilities = api.setBaseUrl("api/facilities/facilities_list/");
-        this.facility_detail = api.setBaseUrl("api/facilities/facilities/");
-        this.facility_types = api.setBaseUrl("api/facilities/facility_types/");
-        this.facility_owners = api.setBaseUrl("api/facilities/owners/");
-        this.regulating_bodies = api.setBaseUrl("api/facilities/regulating_bodies/");
-        this.facility_regulation_status = api.setBaseUrl(
-            "api/facilities/facility_regulation_status/"
-        );
-        this.regulation_status = api.setBaseUrl("api/facilities/regulation_status/");
-        this.wards = api.setBaseUrl("api/common/wards/");
-        this.towns = api.setBaseUrl("api/common/towns");
-        this.officers = api.setBaseUrl("api/facilities/officers/");
-        this.contact_types = api.setBaseUrl("api/common/contact_types/");
-        this.contacts = api.setBaseUrl("api/common/contacts/");
-        this.physical_addresses = api.setBaseUrl("api/common/address/");
-        this.facility_contacts = api.setBaseUrl("api/facilities/contacts/");
-        this.services = api.setBaseUrl("api/facilities/services/");
-        this.service_options = api.setBaseUrl("api/facilities/service_options/");
-        this.facility_services = api.setBaseUrl("api/facilities/facility_services/");
-        this.facility_updates = api.setBaseUrl("api/facilities/facility_updates/");
-        this.operation_status = api.setBaseUrl("api/facilities/facility_status/");
-        this.facility_approvals = api.setBaseUrl("api/facilities/facility_approvals/");
-        this.facility_officers = api.setBaseUrl("api/facilities/facility_officers/");
-        this.facility_upgrade = api.setBaseUrl("api/facilities/facility_upgrade/");
-        this.facility_units = api.setBaseUrl("api/facilities/facility_units/");
-        this.geo_code_methods = api.setBaseUrl("api/gis/geo_code_methods/");
-        this.geo_code_sources = api.setBaseUrl("api/gis/geo_code_sources/");
-        this.facility_coordinates = api.setBaseUrl("api/gis/facility_coordinates/");
-        this.job_titles = api.setBaseUrl("api/facilities/job_titles/");
-        this.create_officer = api.setBaseUrl("api/facilities/officer_facade/");
-        this.keph_levels = api.setBaseUrl("api/facilities/keph/");
-    }]);
+    .service("mfl.facility_mgmt.services.wrappers", ["api", "api.oauth2", "$window",
+        function (api, oauth2, $window) {
+            this.facilities = api.setBaseUrl("api/facilities/facilities_list/");
+            this.facility_detail = api.setBaseUrl("api/facilities/facilities/");
+            this.facility_types = api.setBaseUrl("api/facilities/facility_types/");
+            this.facility_owners = api.setBaseUrl("api/facilities/owners/");
+            this.regulating_bodies = api.setBaseUrl("api/facilities/regulating_bodies/");
+            this.facility_regulation_status = api.setBaseUrl(
+                "api/facilities/facility_regulation_status/"
+            );
+            this.regulation_status = api.setBaseUrl("api/facilities/regulation_status/");
+            this.wards = api.setBaseUrl("api/common/wards/");
+            this.towns = api.setBaseUrl("api/common/towns");
+            this.officers = api.setBaseUrl("api/facilities/officers/");
+            this.contact_types = api.setBaseUrl("api/common/contact_types/");
+            this.contacts = api.setBaseUrl("api/common/contacts/");
+            this.physical_addresses = api.setBaseUrl("api/common/address/");
+            this.facility_contacts = api.setBaseUrl("api/facilities/contacts/");
+            this.services = api.setBaseUrl("api/facilities/services/");
+            this.service_options = api.setBaseUrl("api/facilities/service_options/");
+            this.facility_services = api.setBaseUrl("api/facilities/facility_services/");
+            this.facility_updates = api.setBaseUrl("api/facilities/facility_updates/");
+            this.operation_status = api.setBaseUrl("api/facilities/facility_status/");
+            this.facility_approvals = api.setBaseUrl("api/facilities/facility_approvals/");
+            this.facility_officers = api.setBaseUrl("api/facilities/facility_officers/");
+            this.facility_upgrade = api.setBaseUrl("api/facilities/facility_upgrade/");
+            this.facility_units = api.setBaseUrl("api/facilities/facility_units/");
+            this.geo_code_methods = api.setBaseUrl("api/gis/geo_code_methods/");
+            this.geo_code_sources = api.setBaseUrl("api/gis/geo_code_sources/");
+            this.facility_coordinates = api.setBaseUrl("api/gis/facility_coordinates/");
+            this.job_titles = api.setBaseUrl("api/facilities/job_titles/");
+            this.create_officer = api.setBaseUrl("api/facilities/officer_facade/");
+            this.keph_levels = api.setBaseUrl("api/facilities/keph/");
+
+            this.printFacility = function (facility_id) {
+                var cover_report = api.setBaseUrl("api/facilities/facility_cover_report/");
+                var helpers = api.apiHelpers;
+                var download_params = {
+                    "access_token": oauth2.getToken().access_token
+                };
+                var url = cover_report.makeUrl(
+                    helpers.joinUrl([cover_report.apiBaseUrl, facility_id])
+                );
+                $window.location.href = url + "?" + helpers.makeParams(download_params);
+            };
+        }
+    ]);
 })(window.angular);
--- src/app/facility_mgmt/tests/services.spec.js
+++ src/app/facility_mgmt/tests/services.spec.js
@@ -6,9 +6,20 @@
             module("mflAdminAppConfig");
             module("api.wrapper");
             module("mfl.facility_mgmt.services");
+            module("mfl.auth.oauth2");
         });
 
         describe("Test api wrappers", function() {
+            var $window;
+
+            beforeEach(module(["$provide", function($provide){
+                $window = { location: {href: ""}, localStorage: window.localStorage };
+                $provide.value("$window", $window);
+            }]));
+
+            beforeEach(inject(["api.oauth2", function(oauth2){
+                spyOn(oauth2, "getToken").andReturn({access_token: "123"});
+            }]));
 
             it("should have wrappers defined", function() {
                 inject(["mfl.facility_mgmt.services.wrappers",function(wrappers){
@@ -21,6 +32,16 @@
                     expect(wrappers.officers.apiBaseUrl)
                         .toEqual("api/facilities/officers/");
                 }]);
+            });
+            it("should print cover report", function () {
+                inject(["mfl.facility_mgmt.services.wrappers", "SERVER_URL",
+                    function(wrappers, server_url){
+                        wrappers.printFacility("456");
+                        expect($window.location.href).toEqual(
+                            server_url+"api/facilities/facility_cover_report/456/?access_token=123"
+                        );
+                    }
+                ]);
             });
         });
     });
