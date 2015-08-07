48fd2d046f9f3552b1e6afbbaa2633a8c116c0ba
jason <jason.wanjohi@savannahinformatics.com>
2015-08-07 19:06:19 +0300

    Added test for tiny controller

---
+++ src/app/reports/tests/controllers/main.spec.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+
+    describe("Reports test for main controller", function () {
+
+        beforeEach(function () {
+            module("mflAdminAppConfig");
+            module("mfl.auth.services");
+            module("mfl.reports.controllers");
+            module("ui.router");
+        });
+
+        it("should test header controller", function () {
+            inject(["$controller", "$rootScope", "mfl.auth.services.login",
+                function ($controller, $rootScope, loginService) {
+                    var data = false;
+                    var scope = $rootScope.$new();
+                    $controller("mfl.service_mgmt.controllers.main", {
+                        "$scope": scope,
+                        "mfl.auth.services.login": loginService
+                    });
+
+                    expect(scope.hide_sidebar).toEqual(data);
+                }
+            ]);
+        });
+
+    });
+})();
