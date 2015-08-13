823a92f60b17c2f64a114f2ebd91b2a6658f073c
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-08-13 19:51:06 +0300

    spy on idle functions

--- src/app/auth/controllers.js
+++ src/app/auth/controllers.js
@@ -56,21 +56,21 @@
             $scope.login_err = "";
             $scope.login_err_html = "";
             $scope.params = $stateParams;
+
             $scope.submitUser = function(obj) {
                 $scope.spinner = true;
                 var error_fxn = function (data) {
                     $scope.spinner = false;
                     $scope.login_err = data.data.error_description ||
-                        data.data.detail ||
-                        "Sorry, a connection error occurred";
-                    $scope.login_err_html =  $sce.trustAsHtml($scope.login_err);
+                                       data.data.detail ||
+                                       "Sorry, a connection error occurred";
+                    $scope.login_err_html = $sce.trustAsHtml($scope.login_err);
                 };
                 var success_fxn = function () {
                     $scope.spinner = false;
+                    Idle.watch();
                     var next_state = $stateParams.next || HOME_PAGE_NAME;
-                    $state.go(next_state).then(function () {
-                        Idle.watch();
-                    });
+                    $state.go(next_state);
                 };
                 loginService.login(obj)
                     .then(
@@ -84,13 +84,17 @@
     ])
 
     .controller("mfl.auth.controllers.logout",
-        ["$state", "mfl.auth.services.login", "Idle", function ($state, loginService, Idle) {
+        ["$scope", "$state", "$stateParams", "mfl.auth.services.login", "Idle",
+        function ($scope, $state, $stateParams, loginService, Idle) {
+            $scope.logout = true;
             var callback = function () {
-                $state.go("login").then(function () {
-                    Idle.unwatch();
+                Idle.unwatch();
+                $state.go("login", {
+                    "timeout": $stateParams.timeout,
+                    "next": $stateParams.next
                 });
             };
-            loginService.logout().then(callback, callback);
+            return loginService.logout().then(callback, callback);
         }]
     );
 
