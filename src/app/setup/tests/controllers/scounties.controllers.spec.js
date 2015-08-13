e1c1c497805acdb8ab551bc066eed745fc5b5af8
jason <jason.wanjohi@savannahinformatics.com>
2015-08-13 19:39:41 +0300

    Testing for sub_counties added

--- src/app/setup/tests/controllers/scounties.controllers.spec.js
+++ src/app/setup/tests/controllers/scounties.controllers.spec.js
@@ -20,8 +20,9 @@
                 var data = {
                     $scope: $scope
                 };
-                createController = function(ctrl, params){
-                    return $controller(ctrl, _.extend(data, params));
+                createController = function(name, params){
+                    return $controller("mfl.setup.controller.sub_counties."+
+                                       name, _.extend(data, params));
 
                 };
             }]);
@@ -33,28 +34,146 @@
 
         it("should have `mfl.setup.controller.sub_counties.list` defined",
            function(){
-                var ctrl = createController("mfl.setup.controller.sub_counties.list", {});
+                var ctrl = createController("list", {});
                 expect(ctrl).toBeDefined();
             });
-        it("should have `mfl.setup.controller.sub_counties.details` defined and call made",
-           function(){
-                var dt = {
-                    $stateParams: {scount_id: 1}
-                };
-                $httpBackend.expectGET(SERVER_URL+"api/common/sub_counties/1/")
-                .respond(200);
-                createController("mfl.setup.controller.sub_counties.details",dt);
-                $httpBackend.flush();
-            });
-        it("should have sub-county data fetch failed",
-           function(){
-                var dt = {
-                    $stateParams: {scount_id: 1}
-                };
-                $httpBackend.expectGET(SERVER_URL+"api/common/sub_counties/1/")
-                .respond(500);
-                createController("mfl.setup.controller.sub_counties.details",dt);
-                $httpBackend.flush();
-            });
+        it("should have edit ctrl defined and calls are done successfully | create  succeeds",
+        inject(["$rootScope",function($rootScope){
+            var data = {
+                "$scope": $rootScope.$new()
+            };
+            var frm = {
+                name:"HOSPITAL",
+                county:"1"
+            };
+            $httpBackend.expectGET(SERVER_URL+"api/common/counties/?fields=id,name&"+
+            "page_size=100").respond(200);
+            createController("edit", data);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.resetExpectations();
+            $httpBackend.expectPOST(SERVER_URL+"api/common/sub_counties/").respond(201);
+            data.$scope.saveFrm(frm);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+        }])
+        );
+        it("should have edit ctrl defined and calls are done successfully | create  fails",
+        inject(["$rootScope",function($rootScope){
+            var data = {
+                "$scope": $rootScope.$new()
+            };
+            var frm = {
+                name:"HOSPITAL",
+                county:"1"
+            };
+            $httpBackend.expectGET(SERVER_URL+"api/common/counties/?fields=id,name&"+
+            "page_size=100").respond(200);
+            createController("edit", data);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.resetExpectations();
+            $httpBackend.expectPOST(SERVER_URL+"api/common/sub_counties/").respond(500);
+            data.$scope.saveFrm(frm);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+        }])
+        );
+        it("should have edit ctrl defined and calls are done successfully | edit succeeds",
+        inject(["$rootScope","mfl.common.forms.changes",function($rootScope, formChanges){
+            var data = {
+                "$scope": $rootScope.$new(),
+                "mfl.common.forms.changes" : formChanges,
+                "$stateParams": {scount_id:1}
+            };
+            var frm = {
+                name:"HOSPITAL",
+                county:"1"
+            };
+            $httpBackend.expectGET(SERVER_URL+"api/common/sub_counties/1/").respond(200);
+            $httpBackend.expectGET(SERVER_URL+"api/common/counties/?fields=id,name&"+
+            "page_size=100").respond(200);
+            createController("edit", data);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.resetExpectations();
+            $httpBackend.expectPATCH(SERVER_URL+"api/common/sub_counties/1/")
+            .respond(200, {"id": 1});
+            spyOn(formChanges, "whatChanged").andReturn({name:"HOSPITAL"});
+            data.$scope.saveFrm(frm);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+        }])
+        );
+        it("should have edit ctrl defined and calls are done successfully | edit fails",
+        inject(["$rootScope","mfl.common.forms.changes",function($rootScope, formChanges){
+            var data = {
+                "$scope": $rootScope.$new(),
+                "mfl.common.forms.changes" : formChanges,
+                "$stateParams": {scount_id:1}
+            };
+            var frm = {
+                name:"HOSPITAL",
+                county:"1"
+            };
+            $httpBackend.expectGET(SERVER_URL+"api/common/sub_counties/1/").respond(200);
+            $httpBackend.expectGET(SERVER_URL+"api/common/counties/?fields=id,name&"+
+            "page_size=100").respond(200);
+            createController("edit", data);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.resetExpectations();
+            $httpBackend.expectPATCH(SERVER_URL+"api/common/sub_counties/1/").respond(500);
+            spyOn(formChanges, "whatChanged").andReturn({name:"HOSPITAL"});
+            data.$scope.saveFrm(frm);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+        }])
+        );
+        it("should have edit ctrl defined and calls are done successfully | edit no changes",
+        inject(["$rootScope","mfl.common.forms.changes",function($rootScope, formChanges){
+            var data = {
+                "$scope": $rootScope.$new(),
+                "mfl.common.forms.changes" : formChanges,
+                "$stateParams": {scount_id:1}
+            };
+            var frm = {
+                name:"HOSPITAL",
+                county:"1"
+            };
+            $httpBackend.expectGET(SERVER_URL+"api/common/sub_counties/1/").respond(200);
+            $httpBackend.expectGET(SERVER_URL+"api/common/counties/?fields=id,name&"+
+            "page_size=100").respond(200);
+            createController("edit", data);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+            spyOn(formChanges, "whatChanged").andReturn({});
+            data.$scope.saveFrm(frm);
+        }])
+        );
+        it("should have edit ctrl defined but calls are fail",
+        inject(["$rootScope",function($rootScope){
+            var data = {
+                "$scope": $rootScope.$new(),
+                "$stateParams": {scount_id:1}
+            };
+            $httpBackend.expectGET(SERVER_URL+"api/common/sub_counties/1/").respond(500);
+            createController("edit", data);
+            $httpBackend.expectGET(SERVER_URL+"api/common/counties/?fields=id,name&"+
+            "page_size=100").respond(500);
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingRequest();
+            $httpBackend.verifyNoOutstandingExpectation();
+        }])
+        );
     });
 })(window._);
