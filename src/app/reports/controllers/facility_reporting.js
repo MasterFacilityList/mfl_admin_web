fcdd6b641b5d8c1de7e90d4d5d14f0beff0e3a5a
jason <jason.wanjohi@savannahinformatics.com>
2015-08-14 20:47:53 +0300

    Refactored controllers to reduce code

--- src/app/reports/controllers/facility_reporting.js
+++ src/app/reports/controllers/facility_reporting.js
@@ -6,124 +6,80 @@
         "mfl.reports.services"
     ])
 
-    .controller("mfl.reports.controllers.facility_counties", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_count_by_county"
-            };
-            wrappers.reporting.filter($scope.filters)
+    .controller("mfl.reports.controllers.helper", ["mfl.reports.services.wrappers",
+        function(wrappers){
+            this.initCtrl = function($scope, report_type, data_param){
+                $scope.filters = {
+                    "report_type":report_type
+                };
+                wrappers.reporting.filter($scope.filters)
                 .success(function (data) {
-                    $scope.county_facilities = data.results;
+                    $scope[data_param] = data.results;
                 })
                 .error(function (err) {
                     $scope.errors = err;
                 });
+            };
+        }
+    ])
+    .controller("mfl.reports.controllers.facility_counties", ["$scope", "$controller",
+        function($scope, $controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_count_by_county", "county_facilities");
         }
     ])
     .controller("mfl.reports.controllers.facility_constituencies", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_count_by_consituency"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.const_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+        "$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_count_by_constituency",
+                            "const_facilities");
         }
     ])
-    .controller("mfl.reports.controllers.facility_types", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_count_by_facility_type"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.type_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+    .controller("mfl.reports.controllers.facility_types", ["$scope","$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_count_by_facility_type",
+                            "type_facilities");
         }
     ])
     .controller("mfl.reports.controllers.facility_owner_categories", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_count_by_owner_category"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.owner_cat_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+        "$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_count_by_owner_category",
+                            "owner_cat_facilities");
         }
     ])
-    .controller("mfl.reports.controllers.facility_owners", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_count_by_owner"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.owner_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+    .controller("mfl.reports.controllers.facility_owners", ["$scope","$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_count_by_owner",
+                            "owner_facilities");
         }
     ])
     .controller("mfl.reports.controllers.keph_levels", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_keph_level_report"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.keph_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+        "$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_keph_level_report",
+                            "keph_facilities");
         }
     ])
     .controller("mfl.reports.controllers.county_facility_types", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_count_by_facility_type_detailed"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.county_types_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+        "$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_count_by_facility_type_detailed",
+                            "county_types_facilities");
         }
     ])
     .controller("mfl.reports.controllers.county_constituencies", ["$scope",
-        "mfl.reports.services.wrappers",
-        function($scope,wrappers){
-            $scope.filters = {
-                "report_type":"facility_constituency_report"
-            };
-            wrappers.reporting.filter($scope.filters)
-                .success(function (data) {
-                    $scope.county_constituencies_facilities = data.results;
-                })
-                .error(function (err) {
-                    $scope.errors = err;
-                });
+        "$controller",
+        function($scope,$controller){
+            var helper = $controller("mfl.reports.controllers.helper");
+            helper.initCtrl($scope, "facility_constituency_report",
+                            "county_constituencies_facilities");
         }
     ]);
 })(window.angular);
