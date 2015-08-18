fce549a2ae486ea3442003281924997756d9bb2c
jason <jason.wanjohi@savannahinformatics.com>
2015-08-18 18:44:08 +0300

    Configured circle.ci to start up frontend server

--- ci_test.sh
+++ ci_test.sh
@@ -3,9 +3,12 @@
 case $CIRCLE_NODE_INDEX in
 0)
     export RUN_SAUCE_TESTS=true
-    grunt test
+    grunt test:unit
     ;;
 1)
-    grunt test
+    grunt test:unit
     ;;
+2)
+    grunt connect:dev & 
+    grunt test:e2e
 esac
