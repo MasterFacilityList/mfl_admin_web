8ced3f9de3c267388f698a576ab3585d8088f9f5
ANTONY <antony.owaga@savannahinformatics.com>
2015-09-22 17:36:43 +0300

    Scafolding editing chul

--- src/app/chul/states/view.js
+++ src/app/chul/states/view.js
@@ -16,6 +16,33 @@
                     }
                 },
                 permission: "chul.view_communityhealthunit"
+            })
+            .state("community_units.edit_unit", {
+                url: "edit/:unit_id/",
+                views: {
+                    "main-content@chul": {
+                        templateUrl : "chul/tpls/chul_edit.tpl.html",
+                        controller: "mfl.chul.controllers.edit_chul"
+                    }
+                }
+            })
+            .state("community_units.edit_unit.basic", {
+                url: "basic/",
+                views: {
+                    "form-view@community_units.edit_unit": {
+                        templateUrl : "chul/tpls/chul_basic.tpl.html",
+                        controller: "mfl.chul.controllers.edit_chul.basic"
+                    }
+                }
+            })
+            .state("community_units.edit_unit.chews", {
+                url: "chews/",
+                views: {
+                    "form-view@community_units.edit_unit": {
+                        templateUrl : "chul/tpls/chul_chews.tpl.html",
+                        controller: "mfl.chul.controllers.edit_chul.chews"
+                    }
+                }
             });
     }]);
 
