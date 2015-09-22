fa92f3627172e7708bd7499f6c3e5f4b80b8be61
jason <jason.wanjohi@savannahinformatics.com>
2015-09-22 11:13:47 +0300

    Added structure for approved and rejected chus

--- src/app/chul/states/list.js
+++ src/app/chul/states/list.js
@@ -16,6 +16,39 @@
                 }
             },
             permission: "chul.view_communityhealthunit"
+        })
+        .state("chu_approve_list", {
+            parent: "chul",
+            url: "^/chu_approve_list/",
+            views: {
+                "main-content@chul": {
+                    templateUrl: "chul/tpls/chul_approve.grid.tpl.html",
+                    controller: "mfl.chul.controllers.units_list"
+                }
+            },
+            permission: "chul.view_communityhealthunit"
+        })
+        .state("chu_approved_recently", {
+            parent: "chul",
+            url: "^/chu_approved_recently/",
+            views: {
+                "main-content@chul": {
+                    templateUrl: "chul/tpls/chul_approved_recently.grid.tpl.html",
+                    controller: "mfl.chul.controllers.units_list"
+                }
+            },
+            permission: "chul.view_communityhealthunit"
+        })
+        .state("chu_reject_list", {
+            parent: "chul",
+            url: "^/chu_reject_list/",
+            views: {
+                "main-content@chul": {
+                    templateUrl: "chul/tpls/chul_rejected.grid.tpl.html",
+                    controller: "mfl.chul.controllers.units_list"
+                }
+            },
+            permission: "chul.view_communityhealthunit"
         });
     }]);
 
