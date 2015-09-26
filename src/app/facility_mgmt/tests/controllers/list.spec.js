7e051c9c1cdde3024d79b6dd086bc0ebe45e6f2a
Chomba Ng'ang'a <chomba@savannahinformatics.com>
2015-09-26 17:18:11 +0300

    fix broken tests

--- src/app/facility_mgmt/tests/controllers/list.spec.js
+++ src/app/facility_mgmt/tests/controllers/list.spec.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    describe("Test facility list controllers", function () {
+    ddescribe("Test facility list controllers", function () {
 
         beforeEach(function () {
             module("mfl.facility_mgmt.controllers");
@@ -28,8 +28,16 @@
         it("should load regulator sync list controller", function () {
             inject(["$controller", "$rootScope",
                 function ($controller, rootScope) {
-                    var data = { "$scope": rootScope.$new() };
+                    var data = {
+                        "$scope": rootScope.$new(),
+                        "mfl.auth.services.login": {
+                            "getUser": jasmine.createSpy().andReturn({
+                                "user_counties": [{"county_code": 45}, {"county_code": 2}]
+                            })
+                        }
+                    };
                     $controller("mfl.facility_mgmt.controllers.regulator_sync", data);
+                    expect(data.$scope.filters.county).toEqual([45, 2]);
                 }
             ]);
         });
