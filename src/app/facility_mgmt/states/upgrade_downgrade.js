25dd790f927ba78dd23d204acc043c0d1baafd04
jason <jason.wanjohi@savannahinformatics.com>
2015-09-30 10:59:07 +0300

    Added facility upgrade and downgrade to facility view

--- src/app/facility_mgmt/states/upgrade_downgrade.js
+++ src/app/facility_mgmt/states/upgrade_downgrade.js
@@ -17,11 +17,31 @@
             },
             permission: "facilities.add_facilitytype,facilities.view_facility"
         })
+        .state("facilities.upgrade", {
+            url: "upgrade/:facility_id/",
+            views: {
+                "main-content@facility_mgmt": {
+                    templateUrl: "facility_mgmt/tpls/facility_up_down_grade.tpl.html",
+                    controller: "mfl.facility_mgmt.controllers.facility_upgrade"
+                }
+            },
+            permission: "facilities.add_facilitytype,facilities.view_facility"
+        })
         .state("facilities.facility_edit.downgrade", {
-            url: "downgrade/",
+            url: "downgrade/:facility_id/",
             views: {
                 "form-view@facilities.facility_edit": {
                     templateUrl: "facility_mgmt/tpls/facility_upgrade_downgrade.tpl.html",
+                    controller: "mfl.facility_mgmt.controllers.facility_downgrade"
+                }
+            },
+            permission: "facilities.add_facilitytype,facilities.view_facility"
+        })
+        .state("facilities.downgrade", {
+            url: "downgrade/:facility_id/",
+            views: {
+                "main-content@facility_mgmt": {
+                    templateUrl: "facility_mgmt/tpls/facility_up_down_grade.tpl.html",
                     controller: "mfl.facility_mgmt.controllers.facility_downgrade"
                 }
             },
